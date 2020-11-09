aa1c3ad5d396013abd5f351e998a09b71a5a8fcc
Glitch (hello-webpage) <none>
2020-11-09 07:34:49 +0000

    🍐💃 Checkpoint ./index.html:24838897/1341 ./d3.v5.min.js:24838897/243027 ./main.js:24838897/325 ./style.css:24838897/225 ./.glitch-assets:24838897/347

---
+++ main.js
@@ -0,0 +1,12 @@
+var d3; // Minor workaround to avoid error messages in editors
+
+// Waiting until document has loaded
+window.onload = () => {
+
+    // YOUR CODE GOES HERE
+    console.log("YOUR CODE GOES HERE");
+
+    // Load the data set from the following URL:
+    // https://imld.de/docs/lehre/ws_18-19/data-vis/data/cars.csv
+
+};
