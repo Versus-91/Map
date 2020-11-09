54677c6bcc984812c27dc28ee708242f1583ace1
Glitch (hello-webpage) <none>
2020-11-09 07:46:48 +0000

    🐓🚉 Checkpoint ./main.js:24838897/324

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ var d3; // Minor workaround to avoid error messages in editors
 // Waiting until document has loaded
 window.onload = () => {
 
-    // YOUR CODE GOES HERE
-    console.log("YOUR CODE GOES HERE");
+  // YOUR CODE GOES HERE
+  console.log("YOUR CODE GOES HERE");
 
-    // Load the data set from the following URL:
-    // https://imld.de/docs/lehre/ws_18-19/data-vis/data/cars.csv
+  // Load the data set from the following URL:
+  // https://imld.de/docs/lehre/ws_18-19/data-vis/data/cars.csv
 
 };
