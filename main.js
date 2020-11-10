9aa162154ef8a23f82b8c893c0a15d2612f58692
Glitch (hello-webpage) <none>
2020-11-10 14:09:51 +0000

    🚏🎫 Checkpoint ./main.js:24838897/91

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ window.onload = () => {
   // YOUR CODE GOES HERE
   console.log("YOUR CODE GOES HERE");
 
-  // Load the data set from the following URL:
-  // https://imld.de/docs/lehre/ws_18-19/data-vis/data/cars.csv
+  // Load the data set from the assets folder
 
 };
