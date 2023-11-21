e957a23c44cbf62471e3ae8c3a9dc6c889c3a0dc
Glitch (hello-webpage) <none>
2023-11-21 12:36:36 +0000

    🕵🐗 Checkpoint ./main.js:57809084/14294

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ window.onload = () => {
   const width = canvas.offsetWidth;
   const height = canvas.offsetHeight;
 
-  // Set a projection for the map. Projection = transform a lat/long on a position on the 2d map.
+  // Set a projection for the US map
   const projection = d3
-    .geoNaturalEarth1()
-    .scale(width / 1.3 / Math.PI)
-    .translate([width / 2, height / 2]);
+    .geoAlbersUsa() // Using Albers USA projection for the US map
+    .translate([width / 2, height / 2])
+    .scale(1000); // You might need to adjust the scale for better display
 
   // Get the 'context'
   const ctx = canvas.getContext("2d");
@@ -25,26 +25,42 @@ window.onload = () => {
   // geographic path generator for given projection and canvas context
   const pathGenerator = d3.geoPath(projection, ctx);
 
-  // Draw a background
-  // ctx.fillStyle = '#ddd';
-  // ctx.fillRect(0, 0, width, height);
-
-  // Load external data and boot
-  d3.json(
-    "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
-  ).then(function (data) {
-    // initialize the path
+  // Load US states GeoJSON data and plot map and points
+  Promise.all([
+    d3.json("https://d3js.org/us-10m.v1.json"),
+    d3.json(
+      "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
+    ),
+  ]).then(function ([us, data]) {
+    // Draw US states map
     ctx.beginPath();
-
-    // Got the positions of the path
-    pathGenerator(data);
-
-    // Fill the paths
+    pathGenerator(topojson.feature(us, us.objects.states));
     ctx.fillStyle = "#999";
     ctx.fill();
-
-    // Add stroke
-    ctx.strokeStyle = "#69b3a2";
+    ctx.strokeStyle = "#fff";
     ctx.stroke();
+
+    // Draw world map in the background
+    ctx.beginPath();
+    pathGenerator(data);
+    ctx.fillStyle = "#ddd";
+    ctx.fill();
+    ctx.strokeStyle = "#ccc";
+    ctx.stroke();
+
+    // Plotting points on the US map
+    let points = [
+      { long: -120.085767, lat: 37.681522 }, // Example points within the US
+      { long: -95.568085, lat: 38.260878 },
+    ];
+
+    ctx.fillStyle = "red"; // Set color for points
+
+    points.forEach((point) => {
+      const [x, y] = projection([point.long, point.lat]); // Project lat/long to x/y
+      ctx.beginPath();
+      ctx.arc(x, y, 5, 0, Math.PI * 2); // Draw a circle for each point
+      ctx.fill();
+    });
   });
 };
