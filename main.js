62525dcf2f00d50c94abcce1c08dd1564390d778
Glitch (hello-webpage) <none>
2023-11-21 13:35:25 +0000

    🌛🌲 Checkpoint ./main.js:57809084/17700

--- main.js
+++ main.js
@@ -4,97 +4,72 @@ var topojson;
 window.onload = async () => {
   // YOUR CODE GOES HERE
   console.log("YOUR CODE GOES HERE");
+  d3.csv(
+    "https://cdn.glitch.com/489e63ad-3e39-4120-8308-827b57d31840%2Fairports.csv?v=1606135278302"
+  ).then(async function (airports) {
+    const us = await d3.json("https://d3js.org/us-10m.v2.json");
+    const states = us.objects.states;
+    const data = topojson.feature(us, us.objects.states).features;
+    const colorScale = d3
+      .scaleSequential(d3.interpolateBlues) // Change interpolateBlues to any other color scheme you prefer
+      .domain([0, d3.max(states.geometries, (d) => parseInt(d.id))]);
+    // Step 3. Draw the SVG.
+    // First let's create an empty SVG with 960px width and 600px height.
+    const width = 960;
+    const height = 600;
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-  // Load the data set from the assets folder:
-  // select the canvas element created in the html.
-  const us = await d3.json("https://d3js.org/us-10m.v2.json");
-  const states = us.objects.states;
-  const data = topojson.feature(us, us.objects.states).features;
-  const colorScale = d3
-    .scaleSequential(d3.interpolateBlues) // Change interpolateBlues to any other color scheme you prefer
-    .domain([0, d3.max(states.geometries, (d) => parseInt(d.id))]);
-  // Step 3. Draw the SVG.
-  // First let's create an empty SVG with 960px width and 600px height.
-  const width = 960;
-  const height = 600;
-  const svg = d3
-    .select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    // Create an instance of geoPath.
+    const path = d3.geoPath();
 
-  // Create an instance of geoPath.
-  const path = d3.geoPath();
+    // Use the path to plot the US map based on the geometry data.
+    svg
+      .append("g")
+      .selectAll("path")
+      .data(data)
+      .enter()
+      .append("path")
+      .style("fill", (d) => {
+        const stateName = d.properties.name; // Assuming the state name property is 'name'
+        const stateValue = states.geometries.find(
+          (s) => s.properties.name === stateName
+        )?.id;
+        // Return the color based on the value using the color scale
+        return stateValue ? colorScale(stateValue) : "gray"; // Default color for missing data
+      })
+      .attr("d", path);
+    
+    airports = airports.filter(m=> m.country === 'USA')  
+    const points =airports.map(m =>({
+      name : m.name,
+      latitude : parseFloat(m.latitude),
+      longitude : parseFloat(m.longitude),
+      description : m.name,
+    }))
+    const stateCapitalElements = svg.selectAll("g").data(points).join("g");
+    const projection = d3
+      .geoAlbersUsa()
+      .scale(1300)
+      .translate([width / 2, height / 2]);
 
-  // Use the path to plot the US map based on the geometry data.
-  svg
-    .append("g")
-    .selectAll("path")
-    .data(data)
-    .enter()
-    .append("path")
-    .style("fill", (d) => {
-      const stateName = d.properties.name; // Assuming the state name property is 'name'
-      const stateValue = states.geometries.find(
-        (s) => s.properties.name === stateName
-      )?.id;
-      // Return the color based on the value using the color scale
-      return stateValue ? colorScale(stateValue) : "gray"; // Default color for missing data
-    })
-    .attr("d", path);
+    const capitalGroups = stateCapitalElements
+      .append("g")
+      .attr("transform", ({ longitude, latitude }) => {
+        return `translate(${projection([longitude, latitude])?.join(",")})`;
+      });
 
-  const points = [
-    {
-      name: "Alabama",
-      description: "Montgomery",
-      latitude: 32.377716,
-      longitude: -86.300568,
-    },
-    {
-      name: "Alaska",
-      description: "Juneau",
-      latitude: 58.301598,
-      longitude: -134.420212,
-    },
-    {
-      name: "Arizona",
-      description: "Phoenix",
-      latitude: 33.448143,
-      longitude: -112.096962,
-    },
-    {
-      name: "Arkansas",
-      description: "Little Rock",
-      latitude: 34.746613,
-      longitude: -92.288986,
-    },
-  ];
-  
-  const stateCapitalElements = svg.selectAll("g").data(points).join("g");
-  const projection = d3
-    .geoAlbersUsa()
-    .scale(1300)
-    .translate([width / 2, height / 2]);
-  
-  const capitalGroups = stateCapitalElements
-    .append("g")
-    .attr(
-      "transform",
-      ({ longitude, latitude }) =>{
-        console.log(longitude, latitude)
-        console.log(projection([longitude, latitude]))
-        return  `translate(${projection([longitude, latitude]).join(",")})`
-      }
-       
-    );
+    capitalGroups.append("circle").attr("r", 2);
 
-  capitalGroups.append("circle").attr("r", 2);
-
-  capitalGroups
-    .append("text")
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 10)
-    .attr("text-anchor", "middle")
-    .attr("y", -6)
-    .text(({ description }) => description);
+    capitalGroups
+      .append("text")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("text-anchor", "middle")
+      .attr("y", -6)
+      .text(({ description }) => description);
+  });
 };
