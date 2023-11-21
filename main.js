de39814da1e3f49cf89c32a3361da4c0857b76c4
Glitch (hello-webpage) <none>
2023-11-21 13:00:12 +0000

    🐪⚾️ Checkpoint ./main.js:57809084/20044 ./index.html:57809084/13

--- main.js
+++ main.js
@@ -7,37 +7,86 @@ window.onload = async () => {
 
   // Load the data set from the assets folder:
   // select the canvas element created in the html.
-const width = 960;
-const height = 600;
-
-// Append an SVG element to the #map div
-const svg = d3.select("#map")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
-
-// Define the projection for the map (Albers USA)
-const projection = d3.geoAlbersUsa()
-  .translate([width / 2, height / 2]) // center the map in the SVG
-  .scale(1100);
-
-// Create a path generator
-const path = d3.geoPath().projection(projection);
-
-// Fetch the US states GeoJSON data
-d3.json("https://d3js.org/us-10m.v1.json").then(function(us) {
-  // Extract the features (states) from the GeoJSON data
+  const canvas = document.getElementById("my_dataviz");
+  const us = await d3.json("https://d3js.org/us-10m.v2.json");
   const states = us.objects.states;
+  const data = topojson.feature(us, us.objects.states).features;
+  const colorScale = d3
+    .scaleSequential(d3.interpolateBlues) // Change interpolateBlues to any other color scheme you prefer
+    .domain([0, d3.max(states.geometries, (d) => parseInt(d.id))]);
+  // Step 3. Draw the SVG.
+  // First let's create an empty SVG with 960px width and 600px height.
+  const width = 960;
+  const height = 600;
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-  // Draw the states on the map
-  svg.append("g")
+  // Create an instance of geoPath.
+  const path = d3.geoPath();
+
+  // Use the path to plot the US map based on the geometry data.
+  svg
+    .append("g")
     .selectAll("path")
-    .data(topojson.feature(us, states).features)
-    .enter().append("path")
-    .attr("d", path)
-    .attr("fill", function(d) {
-      // Example: Color the states randomly
-      return d3.rgb(Math.random() * 255, Math.random() * 255, Math.random() * 255);
-    });
-});
+    .data(data)
+    .enter()
+    .append("path")
+    .style("fill", (d) => {
+      const stateName = d.properties.name; // Assuming the state name property is 'name'
+      // Find the corresponding data value for the state
+      console.log(stateName);
+
+      console.log(states.geometries);
+
+      const stateValue = states.geometries.find(
+        (s) => s.properties.name === stateName
+      )?.id;
+      // Return the color based on the value using the color scale
+      console.log(stateValue);
+      return stateValue ? colorScale(stateValue) : "gray"; // Default color for missing data
+    })
+    .attr("d", path);
+
+  // Use the path to plot the US map based on the geometry data.
+  d3.select(svg)
+    .append("g")
+    .selectAll("path")
+    .data(data)
+    .enter()
+    .append("path")
+    .attr("d", path);
+  // Define your points with their coordinates
+  const points = [
+    { name: "Point 1", coordinates: [ 40.7128, -74.0060] },
+    { name: "Point 2", coordinates: [34.0522, -118.2437] },
+    // Add more points as needed
+  ];
+
+  // Add points to the map as circles
+  svg
+    .selectAll(".point")
+    .data(points)
+    .enter()
+    .append("circle")
+    .attr("class", "point")
+    .attr("r", 5) // Set the radius of the circle
+    .attr("cx", (d) => projection(d.coordinates)[0]) // Calculate the X position
+    .attr("cy", (d) => projection(d.coordinates)[1]) // Calculate the Y position
+    .style("fill", "red")
+    .style("opacity", 0.8);
+
+  // You can add labels or tooltips for the points if needed
+  svg
+    .selectAll(".point-label")
+    .data(points)
+    .enter()
+    .append("text")
+    .attr("class", "point-label")
+    .attr("x", (d) => projection(d.coordinates)[0] + 10) // Adjust label position
+    .attr("y", (d) => projection(d.coordinates)[1] + 5) // Adjust label position
+    .text((d) => d.name)
+    .style("fill", "black");
 };
